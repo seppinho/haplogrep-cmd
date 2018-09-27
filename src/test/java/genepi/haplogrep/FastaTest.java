f3fdee8a3cf33a743db0f7d729ffa3adab24095b
Sebastian Schoenherr <sebastian.schoenherr@i-med.ac.at>
2018-09-27 12:30:15 +0200

    Add more Fasta tests

--- src/test/java/genepi/haplogrep/FastaTest.java
+++ src/test/java/genepi/haplogrep/FastaTest.java
@@ -1,19 +1,14 @@
-package genepi.haplogrep;
 
 import static org.junit.Assert.*;
 
 import java.io.File;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.HashSet;
-import java.util.Set;
 
 import org.junit.Test;
 
 import importer.FastaImporter;
 import importer.FastaImporter.References;
-import importer.VcfImporter;
-import junit.framework.Assert;
 
 public class FastaTest {
 
@@ -66,4 +61,115 @@ public class FastaTest {
 
 	}
 
+	@Test
+	public void parseSampleWithDeletions() throws Exception {
+		String file = "test-data/fasta/AY195749.fasta";
+		StringBuilder actual = new StringBuilder();
+		FastaImporter impFasta = new FastaImporter();
+		ArrayList<String> samples = impFasta.load(new File(file), References.RSRS);
+
+		String[] splits = samples.get(0).split("\t");
+
+		boolean deletion = false;
+
+		for (int i = 3; i < splits.length; i++) {
+			if (splits[i].equals("523d")) {
+				deletion = true;
+			}
+			actual.append(splits[i] + ",");
+		}
+
+		assertEquals(true, deletion);
+
+	}
+
+	@Test
+	public void parseSampleWithInsertionsDeletions() throws Exception {
+		String file = "test-data/fasta/InsertionTest.fasta";
+		FastaImporter impFasta = new FastaImporter();
+		ArrayList<String> samples = impFasta.load(new File(file), References.RCRS);
+
+		String[] splits = samples.get(0).split("\t");
+		HashSet<String> set = new HashSet<String>();
+		
+		for (int i = 3; i < splits.length; i++) {
+			set.add(splits[i]);
+		}
+
+		assertEquals(true, set.contains("16182.1C"));
+		assertEquals(true, set.contains("309.1C"));
+		assertEquals(true, set.contains("309.2C"));
+		assertEquals(true, set.contains("309.3T"));
+		assertEquals(true, set.contains("3106d"));
+		assertEquals(true, set.contains("8270d"));
+		assertEquals(true, set.contains("8271d"));
+		assertEquals(true, set.contains("8272d"));
+		assertEquals(true, set.contains("8273d"));
+		assertEquals(true, set.contains("8274d"));
+		assertEquals(true, set.contains("8275d"));
+		assertEquals(true, set.contains("8276d"));
+		assertEquals(true, set.contains("8277d"));
+
+	}
+	
+	// copied first two lines of fasta (including 309.1C etc to end of line)
+	@Test
+	public void parseSampleWithInsertionsDeletionsShuffle() throws Exception {
+		String file = "test-data/fasta/InsertionTest2.fasta";
+		FastaImporter impFasta = new FastaImporter();
+		ArrayList<String> samples = impFasta.load(new File(file), References.RCRS);
+
+		String[] splits = samples.get(0).split("\t");
+		HashSet<String> set = new HashSet<String>();
+		
+		for (int i = 3; i < splits.length; i++) {
+			set.add(splits[i]);
+		}
+
+		assertEquals(true, set.contains("16182.1C"));
+		assertEquals(true, set.contains("309.1C"));
+		assertEquals(true, set.contains("309.2C"));
+		assertEquals(true, set.contains("309.3T"));
+		assertEquals(true, set.contains("3106d"));
+		assertEquals(true, set.contains("8270d"));
+		assertEquals(true, set.contains("8271d"));
+		assertEquals(true, set.contains("8272d"));
+		assertEquals(true, set.contains("8273d"));
+		assertEquals(true, set.contains("8274d"));
+		assertEquals(true, set.contains("8275d"));
+		assertEquals(true, set.contains("8276d"));
+		assertEquals(true, set.contains("8277d"));
+
+	}
+	
+	// random shuffle
+	@Test
+	public void parseSampleWithInsertionsDeletionsShuffle2() throws Exception {
+		String file = "test-data/fasta/InsertionTest3.fasta";
+		FastaImporter impFasta = new FastaImporter();
+		ArrayList<String> samples = impFasta.load(new File(file), References.RCRS);
+
+		String[] splits = samples.get(0).split("\t");
+		HashSet<String> set = new HashSet<String>();
+		
+		for (int i = 3; i < splits.length; i++) {
+			set.add(splits[i]);
+		}
+
+		assertEquals(true, set.contains("16182.1C"));
+		assertEquals(true, set.contains("309.1C"));
+		assertEquals(true, set.contains("309.2C"));
+		assertEquals(true, set.contains("309.3T"));
+		assertEquals(true, set.contains("3106d"));
+		assertEquals(true, set.contains("8270d"));
+		assertEquals(true, set.contains("8271d"));
+		assertEquals(true, set.contains("8272d"));
+		assertEquals(true, set.contains("8273d"));
+		assertEquals(true, set.contains("8274d"));
+		assertEquals(true, set.contains("8275d"));
+		assertEquals(true, set.contains("8276d"));
+		assertEquals(true, set.contains("8277d"));
+
+	}
+	
 }
