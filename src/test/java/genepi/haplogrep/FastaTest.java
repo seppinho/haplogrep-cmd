21c8b8b56d77f75ddd33ded0a99346589a5ed20e
haansi <hansi.weissensteiner@i-med.ac.at>
2018-10-12 15:37:20 +0200

    Add FastaTest - all HGs
    can be tested with RSRS / rCRS
    first version - needs adaptations for RSRS tree (haplogroups with
    backmutations!)

--- src/test/java/genepi/haplogrep/FastaTest.java
+++ src/test/java/genepi/haplogrep/FastaTest.java
@@ -1,14 +1,30 @@
 package genepi.haplogrep;
+
+
 import static org.junit.Assert.*;
 
+import java.io.BufferedInputStream;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.OutputStream;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
 
 import org.junit.Test;
 
+import core.TestSample;
 import importer.FastaImporter;
 import importer.FastaImporter.References;
+import phylotree.Phylotree;
+import phylotree.PhylotreeManager;
+import search.ranking.HammingRanking;
+import search.ranking.results.RankedResult;
 
 public class FastaTest {
 
@@ -172,4 +188,49 @@ public class FastaTest {
 
 	}
 	
+	
+	@Test
+	public void parsePhylotree17() throws Exception {
+		String file = "test-data/fasta/Phylotree17hgs.zip";
+		String fileTemp="test-data/fasta/temp.fasta";
+	 	       
+	//	Phylotree phylotree = PhylotreeManager.getInstance().getPhylotree("data/phylotree/phylotree17.xml","data/weights/weights17.txt");
+		Phylotree phylotree = PhylotreeManager.getInstance().getPhylotree("data/phylotree/phylotree17_rsrs.xml","data/weights/weights17_rsrs.txt");
+		
+	        FileInputStream fileInputStream = new FileInputStream(file);
+	        BufferedInputStream bufferedInputStream = new BufferedInputStream(fileInputStream );
+	        ZipInputStream zin = new ZipInputStream(bufferedInputStream);
+	        ZipEntry ze = null;
+	        Map<String, String> differences = new HashMap<String, String>();
+	        while ((ze = zin.getNextEntry()) != null) {
+	        	String expectedHG = ze.getName();
+	        	 OutputStream out = new FileOutputStream(fileTemp);
+	                byte[] buffer = new byte[20000];
+	                int len;
+	                while ((len = zin.read(buffer)) != -1) {
+	                    out.write(buffer, 0, len);
+	                }
+	                out.close();
+	                FastaImporter impFasta = new FastaImporter();
+	               // ArrayList<String> samples = impFasta.load(new File(fileTemp), References.RCRS);
+		        	
+	                ArrayList<String> samples = impFasta.load(new File(fileTemp), References.RSRS);
+	        	
+	        		List<RankedResult> result =  phylotree.search(TestSample.parse(samples.get(0)),new HammingRanking());
+	        		String expected=samples.get(0).split("\t")[0];
+	        		String resulting=result.get(0).getHaplogroup().toString();
+	        		
+	        		if (!expected.equals(resulting))
+	        			{
+	        			differences.put(expected, resulting);
+	        			}
+
+	        }
+	        System.out.println(differences.size());
+	        for (Map.Entry<String, String> entry : differences.entrySet()) {
+	            System.out.println("Expected = " + entry.getKey() + ", Resulting = " + entry.getValue());
+	        }
+	        zin.close();
+		
+	}
 }
