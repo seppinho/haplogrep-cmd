fb0ec669d8c5e266c9856389dd1d083206b38d99
Sebastian Schoenherr <sebastian.schoenherr@i-med.ac.at>
2019-07-05 13:33:04 +0200

    Update release to v.2.1.21

--- src/main/java/genepi/haplogrep/main/Haplogrep.java
+++ src/main/java/genepi/haplogrep/main/Haplogrep.java
@@ -18,7 +18,7 @@ import core.TestSample;
 
 public class Haplogrep extends Tool {
 
-	public static String VERSION = "v2.1.20";
+	public static String VERSION = "v2.1.21";
 
 	public Haplogrep(String[] args) {
 		super(args);
