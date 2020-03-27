d0c2aff515f42a292ee4cd9aa2d7591ae1f4cfe0
seppinho <sebastian.schoenherr@i-md.ac.at>
2020-03-27 21:30:09 +0100

    Add simple distance check tool

---
+++ src/main/java/genepi/CommandLineInterface.java
@@ -0,0 +1,27 @@
+package genepi;
+
+import genepi.base.Toolbox;
+import genepi.distance.DistanceCheck;
+
+public class CommandLineInterface extends Toolbox {
+
+	public CommandLineInterface(String command, String[] args) {
+		super(command, args);
+		printHeader();
+	}
+
+	private void printHeader() {
+		System.out.println();
+		System.out.println("https://haplogrep.i-med.ac.at");
+
+	
+	}
+
+	public static void main(String[] args) throws Exception {
+		CommandLineInterface toolbox = new CommandLineInterface("cloudgene", args);
+		toolbox.addTool("distance", DistanceCheck.class);
+		toolbox.addTool("haplogrep", DistanceCheck.class);
+		toolbox.start();
+
+	}
+}
---
+++ src/main/java/genepi/distance/DistanceCheck.java
@@ -0,0 +1,92 @@
+package genepi.distance;
+
+import genepi.base.Tool;
+import genepi.io.table.reader.CsvTableReader;
+import genepi.io.table.writer.CsvTableWriter;
+import phylotree.Phylotree;
+import phylotree.PhylotreeManager;
+import java.io.IOException;
+import core.Haplogroup;
+
+public class DistanceCheck extends Tool {
+
+	public static String VERSION = "v0.0.1";
+
+	public DistanceCheck(String[] args) {
+		super(args);
+	}
+
+	@Override
+	public void createParameters() {
+		addParameter("in", "input haplogroup");
+		addParameter("out", "out haplogroup");
+	}
+
+	@Override
+	public void init() {
+
+		System.out.println("Welcome to Haplogrep Distance Check " + VERSION);
+		System.out.println("Instiute of Genetic Epidemiology, Medical University of Innsbruck");
+		System.out.println("");
+
+	}
+
+	@Override
+	public int run() {
+
+		String in = (String) getValue("in");
+		String out = (String) getValue("out");
+
+		CsvTableReader reader = new CsvTableReader(in, ';');
+
+		CsvTableWriter writer = new CsvTableWriter(out, ';');
+		
+		int count = 0;
+		while (reader.next()) {
+
+			count++;
+			
+			Phylotree phylotree = PhylotreeManager.getInstance().getPhylotree("phylotree17.xml", "weights17.txt");
+
+			Haplogroup hgMajor = new Haplogroup(reader.getString("hg1"));
+
+			Haplogroup hgMinor = new Haplogroup(reader.getString("hg2"));
+
+			String[] columns = new String[] { "hg1", "hg2", "distance" };
+			writer.setColumns(columns);
+
+			writer.setString("hg1", reader.getString("hg1"));
+			writer.setString("hg2", reader.getString("hg2"));
+			
+			int distance = 0;
+			try {
+				distance = phylotree.getDistanceBetweenHaplogroups(hgMajor, hgMinor);
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				System.err.println("Line " + count + " includes at least one unknown haplogroup: " +  hgMajor + " / " + hgMinor + ".");
+				System.exit(-1);
+			}
+			writer.setInteger("distance", distance);
+			writer.next();
+
+		}
+
+		System.out.println("Distance calculation finished. File written to " + out+ " .");
+		reader.close();
+		writer.close();
+
+		return 0;
+	}
+
+	public static void main(String[] args) throws IOException {
+
+		DistanceCheck haplogrep = new DistanceCheck(args);
+
+		haplogrep = new DistanceCheck(
+				new String[] { "--in", "/home/seb/Desktop/test.txt", "--out", "/home/seb/Desktop/out.txt" });
+
+		haplogrep.start();
+
+	}
+
+}
